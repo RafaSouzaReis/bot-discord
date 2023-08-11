2a84358e6bab19cbbe7d8c0d0e453d8f8990118c
Rafael Augusto De Souza Reis <rafael.as.reis121@gmail.com>
2023-08-11 14:48:15 -0300

    config dotenv top block
    config dotenv top block

--- src/controllers/client-config.js
+++ src/controllers/client-config.js
@@ -2,14 +2,14 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { config } = require('dotenv');
 const commandsLoader = require('./commands-loader');
 
+//config environment variables
+config();
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, bot => {
     console.log(`Ready! Logged in as ${bot.user.tag}`);
 });
-
-//config environment variables
-config();
 
 client.commands = commandsLoader;
 
