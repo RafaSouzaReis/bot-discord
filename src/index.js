eb90cced0731bd8cc2203cd8663cfa0fe6004b34
Rafael Augusto De Souza Reis <rafael.as.reis121@gmail.com>
2023-08-11 14:40:59 -0300

    code refactoring, and controller implementation
    code refactoring, and controller implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { config } = require('dotenv');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, bot => {
-    console.log(`Ready! Logged in as ${bot.user.tag}`);
-});
-
-config();
-
-client.login(process.env.DISCORD_TOKEN);
+const client = require('./controllers/clientConfig');
