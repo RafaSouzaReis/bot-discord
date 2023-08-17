2530cd70fff5093174ad62c29615bfa8db1be9fd
Rafael Augusto De Souza Reis <rafael.as.reis121@gmail.com>
2023-08-17 10:08:45 -0300

    add events-load file and cooldown in commands
    add events-load file and cooldown in commands

--- src/controllers/client-config.js
+++ src/controllers/client-config.js
@@ -1,39 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { config } = require('dotenv');
 const commandsLoader = require('./commands-loader');
+const { loadEvents } = require('./events-loader');
 
 //config environment variables
 config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, bot => {
-    console.log(`Ready! Logged in as ${bot.user.tag}`);
-});
-
 client.commands = commandsLoader;
 
+loadEvents(client);
+
 client.login(process.env.DISCORD_TOKEN);
-
-client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if(!command){
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try{
-        await command.execute(interaction);
-    }catch(error){
-        console.error(error);
-        if(interaction.replied || interaction.deferred){
-            await interaction. followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        }else{
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-        }
-    }
-});
 
 module.exports = client;
